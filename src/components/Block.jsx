4dd756e417618269b3da59c46dddefa197cfc5b5
emazov <emir.almazovich@mail.ru>
2024-08-21 22:19:20 +0600

    init

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,7 +1,7 @@
 const Block = ({ sign, onClick }) => {
 	return (
 		<div className='zodiac-block' onClick={onClick}>
-			<img src={sign.icon} alt={`${sign.name} icon`} />
+			
 			<h3>{sign.name}</h3>
 			<p>{sign.dateRange}</p>
 		</div>
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,8 +1,8 @@
 import Block from './Block';
 
 const zodiacSigns = [
-	{ name: 'Овен', icon: 'aries.png', dateRange: '21 марта - 19 апреля' },
-	{ name: 'Телец', icon: 'taurus.png', dateRange: '20 апреля - 20 мая' },
+	{ name: 'Овен', dateRange: '21 марта - 19 апреля' },
+	{ name: 'Телец', dateRange: '20 апреля - 20 мая' },
 ];
 
 const Grid = () => {
